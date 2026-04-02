0908bd7ebd764039f34934546fac13a27d5d7036
Javohir Toshkanov <toshkanovjavohir200@gmail.com>
2026-04-02 15:38:56 +0500

    tekshirib berasiz aka

--- apps/common/serializers.py
+++ apps/common/serializers.py
@@ -1,12 +1,46 @@
 from rest_framework import serializers
-from .models import Country, Region
+
+from .models import Country, CustomerFeedback, Media, OurInstagramStory, Region, Settings
+
+
+class MediaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Media
+        fields = ["id", "file", "type"]
+
+
+class SettingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Settings
+        fields = ["id", "home_image", "home_title", "home_subtitle"]
+
 
 class CountrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
-        fields = ['name']
+        fields = ["id", "name", "code"]
+
 
 class RegionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Region
-        fields = "__all__"
+        fields = ["id", "name", "region"]
+
+
+class OurInstagramStorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OurInstagramStory
+        fields = ["id", "image", "story_link"]
+
+
+class CustomerFeedbackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerFeedback
+        fields = [
+            "id",
+            "description",
+            "rank",
+            "customer_name",
+            "customer_position",
+            "customer_image",
+        ]
